1a85397f50468bb7ad78b65865d8a00e1c6557f4
yu-ki-yama <framewebwork212@gmail.com>
2019-08-29 18:06:31 +0900

    filterの追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,17 +19,32 @@ class ApplicationController < ActionController::Base
   end
 
   def authenticate_user
+    puts '0000000000000000000000'
+    puts params['controller']
+    puts params['action']
+    puts params.inspect
 
-    # if params["controller"].split("_")[0] == 'admin'
-    #   unless admin_user_signed_in?
-    #     # redirect_to
-    #   end
-    #
-    # elsif params["controller"].split("_")[0] == 'end'
-    #   puts '----------------------'
-    #   puts end_user_signed_in?
-    #
-    # end
+    if params["controller"] == 'end_users/sessions' && params["action"] == 'new' || params["action"] == 'create'
+      # アクセスを許可する
+    elsif params["controller"] == 'admin_users/sessions' && params["action"] == 'new' || params["action"] == 'create'
+      # アクセスを許可する
+    elsif params["controller"] == 'end_items' && params["action"] == 'index' || params["action"] == 'show'
+      # アクセスを許可する
+    elsif params["controller"] == 'end_users/registrations' && params["action"] == 'new' || params["action"] == 'create'
+      # アクセスを許可する
+    elsif params["controller"].split("_")[0] == 'admin'
+
+      unless admin_user_signed_in?
+        redirect_to new_admin_user_session_path
+      end
+
+    elsif params["controller"].split("_")[0] == 'end'
+
+      unless end_user_signed_in?
+        redirect_to new_end_user_session_path
+      end
+
+    end
 
   end
 
